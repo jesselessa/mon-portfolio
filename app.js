39a039ed1b7abc250f4b0786b9ad26f87225019b
jess <jessica.elessa@gmail.com>
2022-03-26 06:25:40 +0100

    text animation in homepage

--- app.js
+++ app.js
@@ -18,3 +18,36 @@ if (window.matchMedia("(max-width: 1300px)")) {
     });
   });
 }
+
+// Text animation
+
+const txtAnim = document.querySelector(".txt-animation");
+
+let typewriter = new Typewriter(txtAnim, {
+  loop: false,
+  deleteSpeed: 20,
+});
+
+typewriter
+  .pauseFor(1800)
+  .changeDelay(20)
+  .typeString("Je suis Jessica ELESSA")
+  .pauseFor(300)
+  .typeString("<strong>, Développeuse Full Stack</strong> ")
+  .pauseFor(1000)
+  .deleteChars(11)
+  .typeString('<span style="color: #27ae60;"><strong>CSS</strong></span>')
+  .pauseFor(1000)
+  .deleteChars(3)
+  .typeString('<span style="color: #EA39ff;"><strong>React</strong></span> ')
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString(
+    '<span style="color: midnightblue;"><strong>React Native</strong></span> '
+  )
+  .pauseFor(1000)
+  .deleteChars(14)
+  .typeString(
+    '<span style="color: #ff6910;"><strong>JavaScript</strong></span>'
+  )
+  .start();
